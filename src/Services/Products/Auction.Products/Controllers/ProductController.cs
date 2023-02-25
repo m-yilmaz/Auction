79bf992c144353fd72783770a3d1b2554b018be9
yilmazmustafa93 <yilmazmustafa93@gmail.com>
2023-02-25 13:38:55 +0300

    ProductController Crud aksiyonları eklendi.

--- src/Services/Products/Auction.Products/Controllers/ProductController.cs
+++ src/Services/Products/Auction.Products/Controllers/ProductController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.Runtime.Internal.Util;
+using Auction.Products.Entities;
+using Auction.Products.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ZstdSharp.Unsafe;
 
 namespace Auction.Products.Controllers
 {
@@ -8,8 +16,71 @@ namespace Auction.Products.Controllers
     public class ProductController : ControllerBase
     {
 
+        #region Variables
+
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<ProductController> _logger;
+
+        #endregion
+
+        #region Constructor
+
+        public ProductController(IProductRepository productRepository, ILogger<ProductController> logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region Crud_Actions
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        // Birden fazla değeri aynı anda dönmek için kullanılan bir attribute
+        // Product nesnesi ve httpStatus code döndük.
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProduct(string id)
+        {
+            var produrc = await _productRepository.GetProductAsync(id);
+            if (produrc == null)
+            {
+                _logger.LogError($"Product with id : {id}, hasn't been not found in database");
+                return NotFound();
+            }
+            return Ok(produrc);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.Create(product);
+            return CreatedAtRoute("GetProeduct", new { id = product.Id, product });
+        }
 
 
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.Update(product));
+        }
 
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _productRepository.Delete(id));
+        }
+        #endregion
     }
 }

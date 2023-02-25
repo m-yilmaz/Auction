24096beadaa11b0ec8e0ba6f2f1652dc89fbd133
yilmazmustafa93 <yilmazmustafa93@gmail.com>
2023-02-25 11:36:16 +0300

    Add project files.

---
+++ src/Services/Products/Auction.Products/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.Products.Controllers
+{
+    [Route("api/v1[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+
+
+    }
+}

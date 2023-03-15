9ab60cb24a8ac85e0a5c29eceec860fb9432c27a
yilmazmustafa93 <yilmazmustafa93@gmail.com>
2023-03-15 10:01:59 +0300

    Order service operations.

---
+++ src/Services/Order/Ordering.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Domain.Entities;
+using Ordering.Domain.Repositories.Base;
+
+namespace Ordering.Domain.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        // Solid'in liskov ve open/closed a uygun.
+        Task<IEnumerable<Order>> GetOrdersBySellerUserName(string userName);
+    }
+}
---
+++ src/Services/Order/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
